2885d9d39b845814a2ec9321ecd9668630b933bf
Andrew Steurer <andrew.w.steurer@gmail.com>
2024-01-22 10:16:46 -0700

    Initial commit.

---
+++ HashMap.py
@@ -0,0 +1,48 @@
+class HashMap:
+    def __init__(self):
+        self.map = [[None for _ in range(6)] for _ in range(16)] 
+
+    def hash_function(self, key, type=None):
+        return key % 16
+
+    def set_value(self, key, value, type=None):
+        index = self.hash_function(key)
+       
+        if type == None:
+            if isinstance(value, list) and len(value) == 6:
+                self.map[index] = value
+            else:
+                print("If type is set to None, the value needs to be a list type of length 6.")
+        if type == "address":
+            self.map[index][0] = value
+        if type == "deadline":
+            self.map[index][1] = value
+        if type == "city":
+            self.map[index][2] = value
+        if type == "zip":
+            self.map[index][3] = value
+        if type == "weight":
+            self.map[index][4] = value
+        if type == "status":
+            self.map[index][5] = value
+
+        
+
+    def get_value(self, key, type=None):
+        index = self.hash_function(key)
+        if type == None: 
+            return self.map[index]
+        else:
+            if type == "address":
+                return self.map[index][0]
+            if type == "deadline":
+                return self.map[index][1]
+            if type == "city":
+                return self.map[index][2]
+            if type == "zip":
+                return self.map[index][3]
+            if type == "weight":
+                return self.map[index][4]
+            if type == "status":
+                return self.map[index][5]
+
---
+++ Main.py
@@ -0,0 +1,3 @@
+from HashMap import HashMap
+
+
