c619dda00748a788115366552cfa2530fe0f660c
Andrew Steurer <andrew.w.steurer@gmail.com>
2024-01-23 08:50:26 -0700

    More changes

--- Main.py
+++ Main.py
@@ -9,11 +9,8 @@ def deliver_packages():
 def get_total_mileage():
     print("function: total mileage")
 
-def get_delivery_status(start_time, end_time):
+def get_delivery_status(start_end_time_list):
     print("function: delivery status")
-
-import re
-from datetime import datetime
 
 def convert_to_military_time(input_time):
     # Regular expression to match the format 'hour:minute am/pm'
@@ -28,38 +25,48 @@ def convert_to_military_time(input_time):
         # If parsing fails, the time is invalid
         return False
 
+def get_time_input():
+    print("Please enter a start time in the format '03:00 pm' or '3:00 pm'")
+    start_time = None
+    while True:
+        start_time = convert_to_military_time(input("Start time: "))
+        if start_time:
+            break
+        else:
+            print("Please enter a valid start time.")
+
+    print("Please enter an end time in the format '03:00 pm' or '3:00 pm'")
+    end_time = None
+    while True:
+        end_time = convert_to_military_time(input("End time: "))
+        if end_time:
+            break
+        else:
+            print("Please enter a valid end time.")
+
+    return [start_time, end_time]
+
 
 
 def main():
     print(
-        """
-        Please enter the number of the function you want to run:
-        1. View the delivery status of a package
-        2. Total mileage driven by all trucks
-        3. Deliver packages
-        """
+"""
+Please enter the number of the function you want to run:
+1. View the delivery status of a package
+2. Total mileage driven by all trucks
+3. Deliver packages
+"""
         )
     
     function_to_run= input("Please enter 1, 2, 3, or any other key to cancel: ")
 
     if function_to_run in ["1", "2", "3"]:
         if function_to_run == "1":
-            print("Please enter a start time in the format '12:00 pm'")
-            start_time = None
-            while True:
-                start_time = convert_to_military_time(input("Start time: "))
-                if start_time:
-                    break
-                else:
-                    print("Please enter a valid start time.")
-
-            print("Please enter an end time in the format '12:00 pm'")
-            end_time = None
-            while True:
-                end_time = convert_to_military_time(input("End time: "))
-                if end_time:
-                    break
-                else:
-                    print("Please enter a valid end time.")
+            deliver_packages(get_time_input())
+        elif function_to_run == "2":
+            get_total_mileage(get_time_input())
+        elif function_to_run == "3":
+            deliver_packages()
+            
 
 main()
